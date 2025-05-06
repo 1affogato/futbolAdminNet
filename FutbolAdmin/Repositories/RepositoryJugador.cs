acdb0a739a6d6ab26c068a0482297dbfa8fe7399
Josué Torres <josue.ger.torres.gar@gmail.com>
2025-05-05 23:40:55 -0600

    Terminado RepositoryEvento

--- FutbolAdmin/Repositories/RepositoryJugador.cs
+++ FutbolAdmin/Repositories/RepositoryJugador.cs
@@ -3,9 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FutbolAdmin.Model;
 
 namespace FutbolAdmin.Repositories {
-    internal class RepositoryJugador : RepositoryBase {
+    internal class RepositoryJugador : RepositoryBase, ICrud<JugadorModel> {
+        public void Add(JugadorModel entity) {
+            throw new NotImplementedException();
+        }
 
+        public void Delete(int id) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<JugadorModel> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public JugadorModel GetById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public void Update(JugadorModel entity) {
+            throw new NotImplementedException();
+        }
     }
 }
