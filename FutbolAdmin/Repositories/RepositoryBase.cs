a6d59307367e6f6104aa18bc651e312234a0ac26
Josué Torres <josue.ger.torres.gar@gmail.com>
2025-05-05 21:59:34 -0600

    Añadido RepositoryBase

---
+++ FutbolAdmin/Repositories/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace FutbolAdmin.Repositories {
+    internal class RepositoryBase {
+
+        private readonly string _connectionString;
+
+        public RepositoryBase() {
+            // TODO: _connectionString = "";
+        }
+
+        protected SqlConnection GetConnection() {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
