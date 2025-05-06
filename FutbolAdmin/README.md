c1bd08daf39d6db02489482e5e2b4f41d595c6c3
Josué Torres <josue.ger.torres.gar@gmail.com>
2025-05-05 22:47:54 -0600

    Avances en RepositoryBase

---
+++ FutbolAdmin/README.md
@@ -0,0 +1,11 @@
+﻿# FutbolAdminDotnet
+
+### Conectando a la base de datos
+
+1. Es necesario tener una base de datos Oracle database instalada de forma local y crear el usuario que va a acceder a la base de datos del proyecto.
+2. Con ese usuario necesitas ejecutar el script de creación de tablas que se encuentra en la carpeta Scripts del proyecto. El script se llama `FutbolAdmin.sql`.
+3. Necesitas ir al archivo App.config en la raíz del proyecto, ir hasta el final del archivo y cambiar la cadena de conexión por la de tu base de datos local.
+La cadena de conexión tiene el siguiente formato:
+```xml
+connectionString="User Id=TU_USUARIO;Password=TU_CONTRASEÑA_DEL_USUARIO;Data Source=localhost:1521/ORCLPDB1;"
+```
--- FutbolAdmin/Repositories/RepositoryBase.cs
+++ FutbolAdmin/Repositories/RepositoryBase.cs
@@ -5,18 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Data.SqlClient;
+using System.Data.Entity;
+
+using FutbolAdmin.Model;
 
 namespace FutbolAdmin.Repositories {
-    internal class RepositoryBase {
+    internal class RepositoryBase : DbContext {
 
-        private readonly string _connectionString;
-
-        public RepositoryBase() {
+        // La string de conexión está en App.config en la raíz del proyecto
+        public RepositoryBase() : base("name=RepositoryBase") {
             // TODO: _connectionString = "";
         }
 
-        protected SqlConnection GetConnection() {
-            return new SqlConnection(_connectionString);
+        public DbSet<EquipoModel> Equipos { get; set; }
+        public DbSet<JugadorModel> Jugadores { get; set; }
+        public DbSet<PartidoModel> Partidos { get; set; }
+        public DbSet<EventoModel> Eventos { get; set; }
+        public DbSet<TipoEventoModel> TiposEventos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.HasDefaultSchema("MYSCHEMA");
+
+
         }
     }
 }
