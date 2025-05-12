e1e709a6f006f4a600b2f8e5569aa447e9611eaa
Josué Torres <josue.ger.torres.gar@gmail.com>
2025-05-12 00:10:24 -0600

    Añadido ViewModel para crear y consultar jugadores

---
+++ FutbolAdmin/ViewModel/Crud/Jugador/ConsultarJugadorViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FutbolAdmin.Model;
+using FutbolAdmin.Repositories;
+
+namespace FutbolAdmin.ViewModel.Crud.Jugador {
+    public class ConsultarJugadorViewModel {
+        protected RepositoryJugador _repositorioJugador;
+
+        public ConsultarJugadorViewModel() {
+            _repositorioJugador = new RepositoryJugador();
+        }
+
+        public string _playerSearchName { get; set; }
+        public string PlayerSearchName {
+            get => _playerSearchName;
+            set {
+                _playerSearchName = value;
+                // NotifyPropertyChanged("PlayerSearchName");
+            }
+        }
+
+        public ObservableCollection<JugadorModel> Jugadores {
+            get => SearchJugadores();
+        }
+
+        public ObservableCollection<JugadorModel> SearchJugadores() {
+            var jugadores = _repositorioJugador.GetAll();
+            var filteredJugadores = jugadores.Where(j => j.Nombre.ToLower().Contains(PlayerSearchName.ToLower()));
+            return new ObservableCollection<JugadorModel>(filteredJugadores);
+        }
+    }
+}
---
+++ FutbolAdmin/ViewModel/Visitantes/Crud/Jugador/CrearJugadorViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using FutbolAdmin.Model;
+using FutbolAdmin.Repositories;
+
+namespace FutbolAdmin.ViewModel.Crud.Jugador {
+    public class CrearJugadorViewModel : ViewModelBase {
+        protected RepositoryJugador _jugadorRepository;
+        protected RepositoryEquipo _equipoRepository;
+
+        public ICommand CrearJugadorCommand;
+
+        public CrearJugadorViewModel() {
+            _jugadorRepository = new RepositoryJugador();
+            _equipoRepository = new RepositoryEquipo();
+            CrearJugadorCommand = new ComandoViewModel(execute => CrearJugador());
+        }
+
+        private string _errorMessage;
+        public string ErrorMessage {
+            get { return _errorMessage; }
+            set {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+
+        private string _nombreJugador;
+        public string NombreJugador {
+            get { return _nombreJugador; }
+            set {
+                _nombreJugador = value;
+                OnPropertyChanged(nameof(NombreJugador));
+            }
+        }
+
+        private int _edadJugador;
+        public int EdadJugador {
+            get { return _edadJugador; }
+            set {
+                _edadJugador = value;
+                OnPropertyChanged(nameof(EdadJugador));
+            }
+        }
+
+        private int _equipoJugador;
+        public int EquipoJugador {
+            get { return _equipoJugador; }
+            set {
+                _equipoJugador = value;
+                OnPropertyChanged(nameof(EquipoJugador));
+            }
+        }
+
+        public void CrearJugador() {
+            if (string.IsNullOrEmpty(NombreJugador) || EdadJugador <= 0 || EquipoJugador <= 0) {
+                ErrorMessage = "Por favor, complete todos los campos correctamente.";
+                return;
+            }
+            var nuevoJugador = new JugadorModel {
+                Nombre = NombreJugador,
+                Edad = EdadJugador,
+                Equipo = _equipoRepository.GetById(EquipoJugador),
+            };
+            _jugadorRepository.Add(nuevoJugador);
+            MessageBox.Show("Jugador creado exitosamente.");
+        }
+    }
+}
