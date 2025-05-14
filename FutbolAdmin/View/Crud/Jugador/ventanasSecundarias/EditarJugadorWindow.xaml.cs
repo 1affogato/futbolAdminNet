da7dc4f30d15b716c101c153ec7bdf670ae05a86
Carlos Alberto Rodriguez Garcia <rodriguezcarlosalberto73@gmail.com>
2025-05-13 20:44:10 -0600

    FIX: Ahora al regresar de la ventana de editar jugador la ventana de modificar actualiza los valores

--- FutbolAdmin/View/Crud/Jugador/ventanasSecundarias/EditarJugadorWindow.xaml.cs
+++ FutbolAdmin/View/Crud/Jugador/ventanasSecundarias/EditarJugadorWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using FutbolAdmin.Model;
+using FutbolAdmin.ViewModel.Crud.Jugador;
 using FutbolAdmin.ViewModel.Crud.Jugador.VentanasSecundarias;
 
 namespace FutbolAdmin.View.Crud.Jugador.ventanasSecundarias {
@@ -29,6 +30,7 @@ namespace FutbolAdmin.View.Crud.Jugador.ventanasSecundarias {
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
+            this.Owner.DataContext = new ModificarJugadorViewModel();
             NavigationHelper.CloseWindow(this);
         }
     }
